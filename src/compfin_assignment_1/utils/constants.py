fe409178ecb2e150ab498861c6df1087850bf040
MarcellSzegedi <marcellvonszeged@gmail.com>
2025-04-14 22:54:01 +0200

    option hedging implemented for same volatility

---
+++ src/compfin_assignment_1/utils/constants.py
@@ -0,0 +1,4 @@
+"""Constants for compfin package."""
+
+FREQUENCY_TO_HOUR = {"hour": 1, "day": 24, "week": 7 * 24, "biweek": 2 * 7 * 24, "month": 728}
+N_HOURS_PER_YEAR = 24 * 7 * 52  # Using week compatible estimation
